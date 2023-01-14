ab108793725549e83975bbd9333ffe3cf7405c18
Rene Schleese <reneschleese@gmail.com>
2023-01-14 17:22:55 +0100

    Rename SampleScene to Game. Set framerate to 60. Add basic ScrollView as base for joystick

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+}

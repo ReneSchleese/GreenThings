4915f3bcc9ee3085d06d7f3dad835b776584b9d8
Rene Schleese <reneschleese@gmail.com>
2023-01-17 23:49:41 +0100

    Add very basic Character script and Unity CharacterController. Make it move by joystick

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private JoystickBehaviour _joystick;
+    private const float SPEED_FAC = 2f;
+
+    private void Awake()
+    {
+        _joystick.Move += OnMove;
+    }
+
+    private void OnMove(Vector2 obj)
+    {
+        Vector3 speed = new Vector3(obj.x, 0f, obj.y) * SPEED_FAC * Time.deltaTime;
+        _characterController.Move(speed);
+    }
+}

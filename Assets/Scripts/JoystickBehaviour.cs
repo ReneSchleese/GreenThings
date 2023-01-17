d6818c188bc10a8d933d26bca8d2e709c45353e1
Rene Schleese <reneschleese@gmail.com>
2023-01-17 23:06:41 +0100

    Animate joystick back to root on EndDrag

--- Assets/Scripts/JoystickBehaviour.cs
+++ Assets/Scripts/JoystickBehaviour.cs
@@ -1,13 +1,23 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class JoystickBehaviour : MonoBehaviour, IDragHandler, IEndDragHandler
+public class JoystickBehaviour : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
 {
     [SerializeField] private RectTransform _stick, _root;
     private const float MAX_RADIUS_IN_PX = 100f;
     private const float DEADZONE_RADIUS_IN_PX = 5f;
     public event Action<Vector2> Move;
+    private Tween _resetTween;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (_resetTween is { active: true })
+        {
+            _resetTween.Kill();
+        }
+    }
 
     public void OnDrag(PointerEventData eventData)
     {
@@ -27,11 +37,20 @@ public class JoystickBehaviour : MonoBehaviour, IDragHandler, IEndDragHandler
 
         float factor = distance / MAX_RADIUS_IN_PX;
         Vector2 delta = factor * directionNormalized;
+        delta = new Vector2(Mathf.Min(1f, delta.x), Mathf.Min(1f, delta.y));
         Move?.Invoke(delta);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("EndDrag");
+        _resetTween = _stick.DOAnchorPos(Vector2.zero, 0.1f).SetEase(Ease.InOutSine);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (_resetTween is { active: true })
+        {
+            _resetTween.Kill();
+        }
     }
 }

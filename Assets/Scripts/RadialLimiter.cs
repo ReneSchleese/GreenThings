b3b81c8f2b15454cf30813d55a8221c9a10954f0
Rene Schleese <reneschleese@gmail.com>
2023-01-17 22:05:38 +0100

    Use joystick scrollRect onValueChanged event to limit, rather than LateUpdate

--- Assets/Scripts/RadialLimiter.cs
+++ Assets/Scripts/RadialLimiter.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RadialLimiter : MonoBehaviour
 {
     [SerializeField] private RectTransform _stick, _root;
+    [SerializeField] private ScrollRect _scrollRect;
     private const float MAX_DISTANCE_IN_PX = 100;
 
-    private void LateUpdate()
+    private void Awake()
+    {
+        _scrollRect.onValueChanged.AddListener(OnJoyStickMoved);
+    }
+
+    private void OnJoyStickMoved(Vector2 delta)
     {
         Vector2 direction = _stick.anchoredPosition - _root.anchoredPosition;
         float distance = direction.magnitude;

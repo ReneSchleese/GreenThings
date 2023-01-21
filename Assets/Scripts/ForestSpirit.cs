2f9705cdb9df0db994e9df8716a34fec86e1bebd
Rene Schleese <reneschleese@gmail.com>
2023-01-21 23:38:33 +0100

    Add ForestSpirit script, material and prefabs. Think about following mechanic in pseudo code

---
+++ Assets/Scripts/ForestSpirit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class ForestSpirit : MonoBehaviour
+{
+    private void Update()
+    {
+        /*
+         * if not following
+         *      if player in range
+         *          move closer to player
+         *          start following player
+         * else if following player
+         *      if needs to catch up
+         *          if is first spirit
+         *              move to player position
+         *          else
+         *              follow last spirit
+         * else if following spirit
+         *      if player in range
+         *          follow player
+         *      if needs to catch up to spirit
+         *          move to spirit position
+         */
+    }
+}

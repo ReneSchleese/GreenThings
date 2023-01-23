394d1fd2bde2c2b87f539f39f2b7a0fa5ce78d89
Rene Schleese <reneschleese@gmail.com>
2023-01-24 00:05:20 +0100

    Increase player speed. Remove unused field

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,18 +5,15 @@ public class PlayerCharacter : MonoBehaviour, IFollowable
 {
     [SerializeField] private CharacterController _characterController;
     [SerializeField] private JoystickBehaviour _joystick;
-    public const float MOVEMENT_SPEED = 4f;
-    private Vector3 _previousWorldPos;
+    public const float MOVEMENT_SPEED = 6f;
 
     private void Awake()
     {
         _joystick.Move += OnMove;
-        _previousWorldPos = WorldPosition;
     }
 
     private void OnMove(Vector2 obj)
     {
-        _previousWorldPos = WorldPosition;
         Speed = new Vector3(obj.x, 0f, obj.y) * MOVEMENT_SPEED;
         _characterController.Move(Speed * Time.deltaTime);
     }

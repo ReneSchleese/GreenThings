4bce4f4c97e2f79cd2fba7f2dfca84b1f2ac1a44
Rene Schleese <reneschleese@gmail.com>
2023-01-23 22:32:39 +0100

    Small cleanup PushHitbox

--- Assets/Scripts/PushHitbox.cs
+++ Assets/Scripts/PushHitbox.cs
@@ -12,23 +12,21 @@ public class PushHitbox : MonoBehaviour
         
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log($"TRIGGERED, other={other.name}");
-        if (other.TryGetComponent(out PushHitbox otherHitbox))
+        if (!other.TryGetComponent(out PushHitbox otherHitbox))
         {
-            Vector3 direction = otherHitbox.WorldPosition - WorldPosition;
-            float distance = Mathf.Abs(direction.magnitude - otherHitbox.Radius - Radius);
-            Debug.Log($"direction={direction}, distance={distance}");
-            //Debug.Assert(distance < 0);
-            otherHitbox.Push(direction.normalized * 0.01f);
-            //Push(-direction.normalized * 0.01f);
+            return;
         }
+        Vector3 direction = otherHitbox.WorldPosition - WorldPosition;
+        float distance = Mathf.Abs(direction.magnitude - otherHitbox.Radius - Radius);
+        //Debug.Log($"direction={direction}, distance={distance}");
+        otherHitbox.Push(direction.normalized * 0.05f);
     }
 
-    public void Push(Vector3 direction)
+    private void Push(Vector3 direction)
     {
         _target.position += direction;
     }
 
-    public Vector3 WorldPosition => _target.position;
-    public float Radius => _capsuleCollider.radius;
+    private Vector3 WorldPosition => _target.position;
+    private float Radius => _capsuleCollider.radius;
 }

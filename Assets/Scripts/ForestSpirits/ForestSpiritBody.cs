3189e392862327b2fd28964856b4a21ac00102e5
Rene Schleese <reneschleese@gmail.com>
2023-03-28 23:12:13 +0200

    Implement basic unfold routine

--- Assets/Scripts/ForestSpirits/ForestSpiritBody.cs
+++ Assets/Scripts/ForestSpirits/ForestSpiritBody.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 
@@ -13,6 +14,9 @@ public class ForestSpiritBody : MonoBehaviour
 
     private Tween _lookTween;
 
+    private Coroutine _stationaryRoutine;
+    private bool _isStationary;
+
     public void SmoothSetPosition(Vector3 position)
     {
         Vector3 currentPosition = transform.position;
@@ -20,6 +24,41 @@ public class ForestSpiritBody : MonoBehaviour
         Speed = (currentPosition - _lastPosition).sqrMagnitude * Time.deltaTime * WALKING_SPEED_FACTOR;
         _animator.SetFloat(WalkingSpeedAnimationId, Speed);
         _lastPosition = currentPosition;
+
+        if (SlowEnoughForStationary())
+        {
+            if (_isStationary)
+            {
+                return;
+            }
+            _stationaryRoutine ??= StartCoroutine(WaitThenTrySetStationary());
+        }
+        else
+        {
+            _isStationary = false;
+            if (_stationaryRoutine != null)
+            {
+                StopStationaryRoutine();
+            }
+        }
+
+        IEnumerator WaitThenTrySetStationary()
+        {
+            Debug.Log("Waiting...");
+            yield return new WaitForSeconds(3);
+            if (SlowEnoughForStationary())
+            {
+                Debug.Log("Unfold");
+                _isStationary = true;
+                _animator.SetTrigger("Unfold");
+                StopStationaryRoutine();
+            }
+        }
+    }
+
+    private bool SlowEnoughForStationary()
+    {
+        return Speed <= 0.2f;
     }
 
     public void SmoothLookAt(Vector3 position)
@@ -55,6 +94,16 @@ public class ForestSpiritBody : MonoBehaviour
         deriv.w -= derivError.w;		
 		
         return new Quaternion(Result.x, Result.y, Result.z, Result.w);
+    }
+
+    private void StopStationaryRoutine()
+    {
+        if (_stationaryRoutine != null)
+        {
+            Debug.Log("Stopped waiting");
+            StopCoroutine(_stationaryRoutine);
+            _stationaryRoutine = null;
+        }
     }
 
     private Vector3 LookAtPos { get; set; }

3876c0810c13cfe124634a78d8a3876f7fd50d65
Rene Schleese <reneschleese@gmail.com>
2023-07-12 00:23:07 +0200

    Greatly reduce unfold complexity

--- Assets/Scripts/ForestSpirits/ForestSpiritActor.cs
+++ Assets/Scripts/ForestSpirits/ForestSpiritActor.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using DG.Tweening;
 using UnityEngine;
 
 public class ForestSpiritActor : MonoBehaviour
@@ -15,66 +13,49 @@ public class ForestSpiritActor : MonoBehaviour
     private Vector3 _lastPosition;
     private Vector3 _posDampVelocity;
     private Quaternion _rotDampVelocity;
-    private Tween _lookTween;
-    private Coroutine _stationaryRoutine;
-    private bool _isStationary;
+
+    private bool _isInUnfoldState;
+    private float _timeStampWhereFast;
 
     public void SmoothSetPosition(Vector3 position)
     {
         Vector3 currentPosition = transform.position;
         transform.position = Vector3.SmoothDamp(currentPosition, position, ref _posDampVelocity, 0.15f);
         Speed = (currentPosition - _lastPosition).magnitude * (1f / Time.deltaTime);
-
-        _animator.SetFloat(AnimationIds.WalkingSpeed, Speed);
         _lastPosition = currentPosition;
-
-        if (SlowEnoughForStationary())
-        {
-            if (_isStationary)
-            {
-                return;
-            }
-            _stationaryRoutine ??= StartCoroutine(WaitThenTrySetStationary());
-        }
-        else
-        {
-            _isStationary = false;
-            if (_stationaryRoutine != null)
-            {
-                StopStationaryRoutine();
-            }
-        }
-
-        IEnumerator WaitThenTrySetStationary()
-        {
-            Debug.Log("Waiting...");
-            yield return new WaitForSeconds(3);
-            if (SlowEnoughForStationary())
-            {
-                Debug.Log("Unfold");
-                _isStationary = true;
-                _animator.SetTrigger(AnimationIds.Unfold);
-                StopStationaryRoutine();
-            }
-        }
+        
+        _animator.SetFloat(AnimationIds.WalkingSpeed, Speed);
+        HandleUnfold();
     }
 
-    private bool SlowEnoughForStationary() => Speed <= 0.02f;
+    private void HandleUnfold()
+    {
+        if (!IsSlowEnoughToUnfold())
+        {
+            _timeStampWhereFast = Time.time;
+            _isInUnfoldState = false;
+        }
 
+        if (!_isInUnfoldState && HasBeenSlowLongEnoughToUnfold())
+        {
+            Unfold();
+        }
+        
+        bool IsSlowEnoughToUnfold() => Speed <= 0.02f;
+        bool HasBeenSlowLongEnoughToUnfold() => Time.time - _timeStampWhereFast > 3.0f;
+    }
+
+    private void Unfold()
+    {
+        _isInUnfoldState = true;
+        _animator.SetTrigger(AnimationIds.Unfold);
+    }
+    
     public void SmoothLookAt(Vector3 position)
     {
         Vector3 targetPos = new(position.x, 0f, position.z);
         Quaternion lookRotation = Quaternion.LookRotation(targetPos - transform.position, Vector3.up);
         transform.rotation = Utils.SmoothDamp(transform.rotation, lookRotation, ref _rotDampVelocity, 0.2f);
-    }
-    
-    private void StopStationaryRoutine()
-    {
-        if (_stationaryRoutine != null)
-        {
-            StopCoroutine(_stationaryRoutine);
-        }
-        _stationaryRoutine = null;
     }
 
     private float Speed { get; set; }

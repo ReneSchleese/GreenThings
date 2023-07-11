239a50dc11e8c5a628c78f5dbfdfd33cb4cbd968
Rene Schleese <reneschleese@gmail.com>
2023-07-11 23:03:18 +0200

    Refactor: Move rotation smoothDamp into new utils class

--- Assets/Scripts/ForestSpirits/ForestSpiritBody.cs
+++ Assets/Scripts/ForestSpirits/ForestSpiritBody.cs
@@ -65,37 +65,9 @@ public class ForestSpiritBody : MonoBehaviour
     {
         Vector3 targetPos = new(position.x, 0f, position.z);
         Quaternion lookRotation = Quaternion.LookRotation(targetPos - transform.position, Vector3.up);
-        transform.rotation = SmoothDamp(transform.rotation, lookRotation, ref _rotDampVelocity, 0.2f);
+        transform.rotation = Utils.SmoothDamp(transform.rotation, lookRotation, ref _rotDampVelocity, 0.2f);
     }
     
-    // Taken from here: https://gist.github.com/maxattack/4c7b4de00f5c1b95a33b
-    public static Quaternion SmoothDamp(Quaternion rot, Quaternion target, ref Quaternion deriv, float time) {
-        if (Time.deltaTime < Mathf.Epsilon) return rot;
-        // account for double-cover
-        var Dot = Quaternion.Dot(rot, target);
-        var Multi = Dot > 0f ? 1f : -1f;
-        target.x *= Multi;
-        target.y *= Multi;
-        target.z *= Multi;
-        target.w *= Multi;
-        // smooth damp (nlerp approx)
-        var Result = new Vector4(
-            Mathf.SmoothDamp(rot.x, target.x, ref deriv.x, time),
-            Mathf.SmoothDamp(rot.y, target.y, ref deriv.y, time),
-            Mathf.SmoothDamp(rot.z, target.z, ref deriv.z, time),
-            Mathf.SmoothDamp(rot.w, target.w, ref deriv.w, time)
-        ).normalized;
-		
-        // ensure deriv is tangent
-        var derivError = Vector4.Project(new Vector4(deriv.x, deriv.y, deriv.z, deriv.w), Result);
-        deriv.x -= derivError.x;
-        deriv.y -= derivError.y;
-        deriv.z -= derivError.z;
-        deriv.w -= derivError.w;		
-		
-        return new Quaternion(Result.x, Result.y, Result.z, Result.w);
-    }
-
     private void StopStationaryRoutine()
     {
         if (_stationaryRoutine != null)

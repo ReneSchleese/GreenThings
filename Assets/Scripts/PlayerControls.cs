198b5f446abf7f960eb89c1e6a13e7ea5af773a5
Rene Schleese <reneschleese@gmail.com>
2023-08-09 19:38:09 +0200

    Add just a rough version of WASD keyboard controls

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private JoystickBehaviour _joystick;
+
+    private void Start()
+    {
+        _joystick = FindObjectOfType<JoystickBehaviour>();
+    }
+
+    private void Update()
+    {
+        Vector2 amount = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _joystick.InvokeMove(amount);
+    }
+}

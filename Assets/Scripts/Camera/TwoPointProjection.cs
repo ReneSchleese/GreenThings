5a4260e61ae531a06b1cab1f0ac0100bf50d0050
Rene Schleese <reneschleese@gmail.com>
2024-02-11 13:26:20 +0100

    add TwoPointProjection stub

---
+++ Assets/Scripts/Camera/TwoPointProjection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TwoPointProjection : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] [Range(0, 360)] private float _degrees;
+
+    private void Update()
+    {
+        
+    }
+}

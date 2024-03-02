914e2d44947a88ca553ae2b1706e149759df4548
Rene Schleese <reneschleese@gmail.com>
2024-03-02 11:09:55 +0100

    move dress with spring

--- Assets/Scripts/HornetDress.cs
+++ Assets/Scripts/HornetDress.cs
@@ -3,5 +3,20 @@ using UnityEngine;
 public class HornetDress : MonoBehaviour
 {
     [SerializeField] private Transform _dressBone;
-    [SerializeField] private SpringJoint _joint;
+    [SerializeField] private Transform _target;
+    [SerializeField] [Range(0, 1)] private float _strength = 0.5f;
+
+    private Vector3 _offset;
+    private Vector3 _initialLocalPos;
+    
+    private void Awake()
+    {
+        _initialLocalPos = _dressBone.localPosition;
+        _offset = _dressBone.position - _target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        _dressBone.position = Vector3.Lerp(_dressBone.parent.TransformPoint(_initialLocalPos), _target.position + _offset, _strength);
+    }
 }

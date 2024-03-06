b2862a1b4cb18e0c50db4f0151e9b0de499e4524
Rene Schleese <reneschleese@gmail.com>
2024-03-06 23:17:36 +0100

    try out animator state functions

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -12,6 +12,7 @@ public class PlayerCharacter : MonoBehaviour, IChainTarget, IPushable
     public Chain Chain;
     public const float MOVEMENT_SPEED = 8f;
     private Vector3 _positionLastFrame;
+    private Vector3 _lastVelocity;
 
     private void Awake()
     {
@@ -24,7 +25,46 @@ public class PlayerCharacter : MonoBehaviour, IChainTarget, IPushable
         _characterController.Move(_characterController.isGrounded ? Vector3.zero : Physics.gravity * Time.deltaTime);
         Velocity = (transform.position - _positionLastFrame) / Time.deltaTime;
         _positionLastFrame = transform.position;
-        _animator.SetFloat("MovementSpeed", Velocity.magnitude);
+
+        if (Velocity != Vector3.zero)
+        {
+            _animator.SetFloat("MovementSpeed", Velocity.magnitude);
+            var stateIsMovement = _animator.GetCurrentAnimatorStateInfo(0).IsName("Movement");
+            var isTransitioningToMovement = _animator.GetAnimatorTransitionInfo(0).IsName("IdleToMovement");
+            var isName = _animator.GetNextAnimatorStateInfo(0).IsName("Movement");
+            if (isName)
+            {
+                Debug.Log("WOULD BE RUN");
+            }
+            if (isTransitioningToMovement)
+            {
+                Debug.Log("TRANSITIONING TO IDLE");
+            }
+            if (!stateIsMovement && !isTransitioningToMovement && !isName)
+            {
+                _animator.ResetTrigger("Idle");
+                _animator.ResetTrigger("Run");
+                Debug.Log("Trigger Run!");
+                _animator.SetTrigger("Run");
+            }
+        }
+        else
+        {
+            
+            var stateIsIdle = _animator.GetCurrentAnimatorStateInfo(0).IsName("Idle");
+            var isTransitioningToIdle = _animator.GetAnimatorTransitionInfo(0).IsName("MovementToIdle");
+            if (isTransitioningToIdle)
+            {
+                Debug.Log("TRANSITIONING TO IDLE");
+            }
+            if (!stateIsIdle && !isTransitioningToIdle)
+            {
+                _animator.ResetTrigger("Idle");
+                _animator.ResetTrigger("Run");
+                Debug.Log("Trigger Idle!");
+                _animator.SetTrigger("Idle");
+            }
+        }
     }
 
     private void OnMove(Vector2 delta)
@@ -37,6 +77,11 @@ public class PlayerCharacter : MonoBehaviour, IChainTarget, IPushable
         {
             transform.rotation = Quaternion.LookRotation(new Vector3(Velocity.x, 0f, Velocity.z));
             Chain.OnUpdate();
+            
+        }
+        else
+        {
+            
         }
     }
 

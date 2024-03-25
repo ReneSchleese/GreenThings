f7226fa7f5c65ca845153beff5887474ecd37a40
Rene Schleese <reneschleese@gmail.com>
2024-03-25 20:35:59 +0100

    footstep events in run animation

---
+++ Assets/Scripts/Hornet/Footsteps.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] _stepsGrass;
+
+    [UsedImplicitly]
+    public void OnFootStep()
+    {
+        Debug.Log("Footstep!");
+    }
+}

91ae6c27216bfd9f42c88d3986c2d3ca72e72817
Rene Schleese <reneschleese@gmail.com>
2024-03-25 19:57:16 +0100

    add level. add ambient track. make level play ambient sound

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -4,7 +4,23 @@ namespace Audio
 {
     public class AudioManager : MonoBehaviour
     {
+        [SerializeField] private AudioSource _ambientSource;
+        [SerializeField] private AudioSource _effectSource;
+        
         private static AudioManager _instance;
+        
+        public void PlayAmbient(AudioClip clip, bool loop)
+        {
+            _ambientSource.clip = clip;
+            _ambientSource.loop = loop;
+            _ambientSource.Play();
+        }
+
+        public void PlayEffect(AudioClip clip)
+        {
+            _effectSource.PlayOneShot(clip);
+        }
+        
         public static AudioManager Instance
         {
             get

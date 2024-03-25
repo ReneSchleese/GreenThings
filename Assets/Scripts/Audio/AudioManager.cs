e0a0617849c1ba3bb6871d7cef155060027e617a
Rene Schleese <reneschleese@gmail.com>
2024-03-25 19:41:20 +0100

    add audioManager class

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        private static AudioManager _instance;
+        public static AudioManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<AudioManager>();
+                }
+                return _instance;
+            }
+        }
+    }
+}

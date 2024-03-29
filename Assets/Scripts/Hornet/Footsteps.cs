0eb59ced3b2e417061f0dbfa309fdff54bd718ca
Rene Schleese <reneschleese@gmail.com>
2024-03-29 22:01:02 +0100

    add pseudo random index class

--- Assets/Scripts/Hornet/Footsteps.cs
+++ Assets/Scripts/Hornet/Footsteps.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Footsteps : MonoBehaviour
 {
     [SerializeField] private AudioClip[] _stepsGrass;
+    private PseudoRandomIndex _pseudoRandom;
+
+    private void Awake()
+    {
+        _pseudoRandom = new PseudoRandomIndex(length: _stepsGrass.Length);
+    }
 
     [UsedImplicitly]
     public void OnFootStep()
     {
         float pitch = Random.Range(0.7f, 1.2f);
-        AudioManager.Instance.PlayEffect(_stepsGrass[Random.Range(0, _stepsGrass.Length)], pitch);
+        AudioManager.Instance.PlayEffect(_stepsGrass[_pseudoRandom.Get()], pitch);
     }
 }

8e8ca43c026232010ea796305b3b241623b01dbc
Rene Schleese <reneschleese@gmail.com>
2024-11-06 22:33:45 +0100

    forgot to add cs file

---
+++ Assets/Scripts/UI/VirtualJoystickRegion.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class VirtualJoystickRegion : MonoBehaviour
+{
+    [SerializeField] private VirtualJoystick _virtualJoystick;
+    public VirtualJoystick VirtualJoystick => _virtualJoystick;
+}

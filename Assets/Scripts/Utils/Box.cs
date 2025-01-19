9056219ada44c92c39151a584be85935e0a2bb2c
Rene Schleese <reneschleese@gmail.com>
2025-01-19 14:52:39 +0100

    work on box script as bounds doesn't seem to work in this case

---
+++ Assets/Scripts/Utils/Box.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public struct Box
+{
+    private readonly Vector3[] _vertices;
+
+    public Box(Vector3[] vertices)
+    {
+        _vertices = vertices;
+    }
+
+    public static Box FromCollider(BoxCollider collider)
+    {
+        Vector3 extends = 0.5f * collider.size;
+        Vector3 lossyScale = collider.transform.lossyScale;
+        Vector3 scaledExtends = new(
+            lossyScale.x * extends.x,
+            lossyScale.y * extends.y,
+            lossyScale.z * extends.z);
+        Vector3 worldCenter = collider.transform.TransformPoint(collider.center);
+        Vector3[] vertices = {
+            worldCenter - Vector3.up * scaledExtends.y - Vector3.right * scaledExtends.x - Vector3.forward * scaledExtends.z,
+            worldCenter - Vector3.up * scaledExtends.y - Vector3.right * scaledExtends.x + Vector3.forward * scaledExtends.z,
+            worldCenter - Vector3.up * scaledExtends.y + Vector3.right * scaledExtends.x + Vector3.forward * scaledExtends.z,
+            worldCenter - Vector3.up * scaledExtends.y + Vector3.right * scaledExtends.x - Vector3.forward * scaledExtends.z,
+            worldCenter + Vector3.up * scaledExtends.y - Vector3.right * scaledExtends.x - Vector3.forward * scaledExtends.z,
+            worldCenter + Vector3.up * scaledExtends.y - Vector3.right * scaledExtends.x + Vector3.forward * scaledExtends.z,
+            worldCenter + Vector3.up * scaledExtends.y + Vector3.right * scaledExtends.x + Vector3.forward * scaledExtends.z,
+            worldCenter + Vector3.up * scaledExtends.y + Vector3.right * scaledExtends.x - Vector3.forward * scaledExtends.z,
+        };
+        return new Box(vertices);
+    }
+
+    public void Draw(Color color, float duration)
+    {
+        Debug.DrawLine(_vertices[0], _vertices[1], color, duration);
+        Debug.DrawLine(_vertices[0], _vertices[3], color, duration);
+        Debug.DrawLine(_vertices[1], _vertices[2], color, duration);
+        Debug.DrawLine(_vertices[3], _vertices[2], color, duration);
+        
+        Debug.DrawLine(_vertices[4], _vertices[5], color, duration);
+        Debug.DrawLine(_vertices[4], _vertices[7], color, duration);
+        Debug.DrawLine(_vertices[5], _vertices[6], color, duration);
+        Debug.DrawLine(_vertices[7], _vertices[6], color, duration);
+        
+        Debug.DrawLine(_vertices[0], _vertices[4], color, duration);
+        Debug.DrawLine(_vertices[1], _vertices[5], color, duration);
+        Debug.DrawLine(_vertices[2], _vertices[6], color, duration);
+        Debug.DrawLine(_vertices[3], _vertices[7], color, duration);
+    }
+}
--- Assets/Scripts/Utils/SpriteBlobShadow.cs
+++ Assets/Scripts/Utils/SpriteBlobShadow.cs
@@ -1,21 +1,43 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class SpriteBlobShadow : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer _blobShadow;
+    [SerializeField] private SpriteRenderer _shadowRenderer;
+    [SerializeField] private BoxCollider _boxCollider;
+    private const float PUSH_UP_Y = 0.2f;
+    private Bounds _bounds;
+    private Box _box;
+
+    private void Awake()
+    {
+        _bounds = _boxCollider.bounds;
+        _box = Box.FromCollider(_boxCollider);
+    }
 
     private void Update()
     {
         UpdateShadow();
+        _box.Draw(Color.red, 0.01f);
     }
 
     private void UpdateShadow()
     {
-        Physics.Raycast(new Ray(_blobShadow.transform.position, Vector3.down), out RaycastHit hit , 2f);
+        Vector3 origin = new(
+            _shadowRenderer.transform.position.x, 
+            _bounds.max.y,
+            _shadowRenderer.transform.position.z);
+        float distance = _bounds.size.y;
+        Physics.Raycast(new Ray(origin, Vector3.down), out RaycastHit hit, distance);
+        Debug.DrawRay(origin, Vector3.down * distance, Color.red);
+        Debug.Log($"origin={origin}, distance={distance}");
         bool isGrounded = hit.collider != null;
         if (isGrounded)
         {
-            _blobShadow.transform.rotation = Quaternion.LookRotation(hit.normal);
+            _shadowRenderer.transform.rotation = Quaternion.LookRotation(hit.normal);
+            Vector3 hitPointInLocal = transform.InverseTransformPoint(hit.point);
+            float y = Mathf.Clamp(hitPointInLocal.y + PUSH_UP_Y, 0f, _bounds.max.y);
+            _shadowRenderer.transform.localPosition = Vector3.up * y;
         }
     }
 }

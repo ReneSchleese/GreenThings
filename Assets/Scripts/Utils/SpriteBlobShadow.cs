cc4b611d58bee6ead59bb913ef71366cb69acc32
Rene Schleese <reneschleese@gmail.com>
2025-01-19 13:22:11 +0100

    add blobshadow script and prefab

---
+++ Assets/Scripts/Utils/SpriteBlobShadow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpriteBlobShadow : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _blobShadow;
+
+    private void Update()
+    {
+        UpdateShadow();
+    }
+
+    private void UpdateShadow()
+    {
+        Physics.Raycast(new Ray(_blobShadow.transform.position, Vector3.down), out RaycastHit hit , 2f);
+        bool isGrounded = hit.collider != null;
+        if (isGrounded)
+        {
+            _blobShadow.transform.rotation = Quaternion.LookRotation(hit.normal);
+        }
+    }
+}

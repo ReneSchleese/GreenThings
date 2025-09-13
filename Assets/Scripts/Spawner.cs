f305f0e17295a29911e89879cc165e7c0035bc44
Rene Schleese <reneschleese@gmail.com>
2025-09-13 12:14:52 +0200

    add mock diggingHole prefab. make spawner spawn it

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,6 +6,7 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Transform _forestSpiritParent;
     [SerializeField] private BuriedTreasure _buriedTreasurePrefab;
     [SerializeField] private Coin _coinPrefab;
+    [SerializeField] private GameObject _diggingHole;
     
     public void SpawnForestSpirit(Vector3 position, Quaternion rotation)
     {
@@ -20,5 +21,10 @@ public class Spawner : MonoBehaviour
     public Coin SpawnCoin(Vector3 position, Quaternion rotation)
     {
         return Instantiate(_coinPrefab, position, rotation);
+    }
+
+    public GameObject SpawnDiggingHole(Vector3 position, Transform parent)
+    {
+        return Instantiate(_diggingHole, position, Quaternion.identity, parent);
     }
 }

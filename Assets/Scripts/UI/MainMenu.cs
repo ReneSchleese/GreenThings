5107a9411b536b0452e38f10627753d420bb7228
Rene Schleese <reneschleese@gmail.com>
2025-09-28 14:07:03 +0200

    add mainMenu script. give it two buttons "start game" and "shop"

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _startGameButton;
+    [SerializeField] private Button _shopButton;
+    
+    void Start()
+    {
+        _startGameButton.onClick.AddListener(OnStartGamePressed);
+        _shopButton.onClick.AddListener(OnShopPressed);
+
+        void OnStartGamePressed()
+        {
+            Debug.Log("Start Game");
+        }
+
+        void OnShopPressed()
+        {
+            Debug.Log("Shop");
+        }
+    }
+}

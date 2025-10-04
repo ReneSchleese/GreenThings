65d19f3867190cbf8c7b92ff500218615ce7232f
Rene Schleese <reneschleese@gmail.com>
2025-10-04 23:45:06 +0200

    get host and api key from environment variables. download bottled-messages.json

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour, IAppState
@@ -24,7 +25,33 @@ public class MainMenu : MonoBehaviour, IAppState
 
         void OnShopPressed()
         {
-            Debug.Log("Shop");
+            StartCoroutine(Download());
+            IEnumerator Download()
+            {
+                string apiKey = System.Environment.GetEnvironmentVariable("GREEN_THINGS_API_KEY");
+                string host = System.Environment.GetEnvironmentVariable("GREEN_THINGS_API_HOST");
+                Debug.Log($"apiKey={apiKey}, host={host}");
+                UnityWebRequest request = UnityWebRequest.Get($"https://{host}/api/bottled-messages");
+                request.SetRequestHeader("x-api-key", apiKey);
+
+                yield return request.SendWebRequest();
+                
+                if (request.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError($"Request failed: {request.error}");
+                }
+                else
+                {
+                    // Get the JSON response as a string
+                    string json = request.downloadHandler.text;
+
+                    Debug.Log("Received JSON:\n" + json);
+
+                    // Optionally parse it into a class/array
+                    // Example: Deserialize into your data model
+                    // var messages = JsonUtility.FromJson<MessageList>(json);
+                }
+            }
         }
     }
 

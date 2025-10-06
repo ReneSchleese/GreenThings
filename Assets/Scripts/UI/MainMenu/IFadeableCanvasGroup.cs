eb5397d39a032f89ecfeabff72c151cc586796ce
Rene Schleese <reneschleese@gmail.com>
2025-10-06 20:42:10 +0200

    fix not setting blocksRaycasts and therefore making the views unclickable

--- Assets/Scripts/UI/MainMenu/IFadeableCanvasGroup.cs
+++ Assets/Scripts/UI/MainMenu/IFadeableCanvasGroup.cs
@@ -8,13 +8,22 @@ public interface IFadeableCanvasGroup
     public Tween Fade(bool fadeIn)
     {
         return CanvasGroup.DOFade(fadeIn ? 1f : 0f, 1f)
-            .OnStart(() => CanvasGroup.interactable = false)
-            .OnComplete(() => CanvasGroup.interactable = fadeIn);
+            .OnStart(() =>
+            {
+                CanvasGroup.interactable = false;
+                CanvasGroup.blocksRaycasts = true;
+            })
+            .OnComplete(() =>
+            {
+                CanvasGroup.interactable = fadeIn;
+                CanvasGroup.blocksRaycasts = fadeIn;
+            });
     }
 
     public void FadeInstantly(bool fadeIn)
     {
         CanvasGroup.alpha = fadeIn ? 1f : 0f;
         CanvasGroup.interactable = fadeIn;
+        CanvasGroup.blocksRaycasts = fadeIn;
     }
 }

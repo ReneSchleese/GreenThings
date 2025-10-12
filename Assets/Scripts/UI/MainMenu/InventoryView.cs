49e33aaee08ddf5cdfeafb3d49ed0371e6ab3181
Rene Schleese <reneschleese@gmail.com>
2025-10-12 11:40:37 +0200

    update inventory items on start and with shop and userData update

--- Assets/Scripts/UI/MainMenu/InventoryView.cs
+++ Assets/Scripts/UI/MainMenu/InventoryView.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,18 +9,41 @@ public class InventoryView : MonoBehaviour, IFadeableCanvasGroup
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private Button _backButton;
     [SerializeField] private InventoryBottleItemView _bottleItemViewPrefab;
+    [SerializeField] private Transform _bottleItemsContainer;
+    
+    private readonly List<InventoryBottleItemView> _bottleItemViews = new();
     
     public event Action BackButtonPress;
 
     public void OnLoad()
     {
-        _backButton.onClick.AddListener(() => BackButtonPress?.Invoke());    
+        _backButton.onClick.AddListener(() => BackButtonPress?.Invoke());
+        App.Instance.UserData.Update += UpdateItems;
+        App.Instance.Shop.Update += UpdateItems;
+        UpdateItems();
     }
 
     public void OnUnload()
     {
-        
+        App.Instance.UserData.Update -= UpdateItems;
+        App.Instance.Shop.Update -= UpdateItems;
     }
-    
+
+    private void UpdateItems()
+    {
+        foreach (InventoryBottleItemView bottleItemView in _bottleItemViews)
+        {
+            Destroy(bottleItemView.gameObject);
+        }
+        _bottleItemViews.Clear();
+        foreach (string messageId in App.Instance.UserData.OwnedMessageIds)
+        {
+            BottledMessageJson messageJson = App.Instance.Shop.Messages.First(message => message.id == messageId);
+            InventoryBottleItemView bottleItemView = Instantiate(_bottleItemViewPrefab,  _bottleItemsContainer);
+            bottleItemView.Set(messageJson);
+            _bottleItemViews.Add(bottleItemView);
+        }
+    }
+
     public CanvasGroup CanvasGroup => _canvasGroup;
 }

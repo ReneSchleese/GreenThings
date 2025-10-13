33d3a5bbe474b1ba47082d82d668a0fc09163ebe
Rene Schleese <reneschleese@gmail.com>
2025-10-13 22:01:44 +0200

    download thumbnail and set it in bottleItemView

--- Assets/Scripts/UI/MainMenu/InventoryBottleItemView.cs
+++ Assets/Scripts/UI/MainMenu/InventoryBottleItemView.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class InventoryBottleItemView : MonoBehaviour
 {
     [SerializeField] private Image _thumbnailImage;
     [SerializeField] private Button _button;
+    [SerializeField] private RawImage _thumbnailRawImage;
     public event Action<InventoryBottleItemView> OnClick;
     private BottledMessageJson _messageJson;
 
@@ -14,6 +17,24 @@ public class InventoryBottleItemView : MonoBehaviour
         _messageJson = messageJson;
         _button.onClick.RemoveAllListeners();
         _button.onClick.AddListener(() => OnClick?.Invoke(this));
+        StartCoroutine(DownloadAndSetRawImage(messageJson.thumbnail_url));
+        
+        IEnumerator DownloadAndSetRawImage(string imageUrl)
+        {
+            using var request = UnityWebRequestTexture.GetTexture(imageUrl);
+            request.SetRequestHeader("x-api-key", BuildConfigLoader.Config.ApiKey);
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError($"Failed to download image: {request.error}");
+            }
+            else
+            {
+                Texture2D texture = DownloadHandlerTexture.GetContent(request);
+                _thumbnailRawImage.texture = texture;
+            }
+        }
     }
 
     public BottledMessageJson Data => _messageJson;

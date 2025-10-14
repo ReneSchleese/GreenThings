10fc6cf753a0db84744cda62a0af90648ead0891
Rene Schleese <reneschleese@gmail.com>
2025-10-14 16:58:15 +0200

    implement DownloadableContent.cs GetTexture which supports local caching, downloading from the cloud and returning from memory

--- Assets/Scripts/DownloadableContent.cs
+++ Assets/Scripts/DownloadableContent.cs
@@ -1,11 +1,108 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class DownloadableContent : MonoBehaviour
 {
-    public Texture2D GetTexture(string url, Action<Texture2D> callback)
+    private readonly Dictionary<string, Texture2D> _textures = new();
+    private readonly Dictionary<string, List<Action<Texture2D>>> _textureRequests = new();
+
+    public void GetTexture(string url, Action<Texture2D> callback)
     {
-        return null;
+        Debug.Log("GetTexture: " + url);
+        bool alreadyInMemory = _textures.TryGetValue(url, out var cached);
+        if (alreadyInMemory)
+        {
+            Debug.Log("alreadyInMemory: " + url);
+            callback?.Invoke(cached);
+            return;
+        }
+
+        bool alreadyDownloading = _textureRequests.TryGetValue(url, out var list);
+        if (alreadyDownloading)
+        {
+            Debug.Log("alreadyDownloading: " + url);
+            list.Add(callback);
+            return;
+        }
+
+        _textureRequests[url] = new List<Action<Texture2D>> { callback };
+        StartCoroutine(LoadFromDiskDownloadOtherwise(url));
+    }
+
+    private IEnumerator LoadFromDiskDownloadOtherwise(string url)
+    {
+        string tempFilePath = ToLocalFilePath(url);
+        CheckDirectory(tempFilePath);
+        Debug.Log("tempFilePath: " + tempFilePath);
+
+        if (!File.Exists(tempFilePath))
+        {
+            using (var webRequest = UnityWebRequest.Get(url))
+            {
+                webRequest.SetRequestHeader("x-api-key", BuildConfigLoader.Config.ApiKey);
+                Debug.Log("downloading: " + url);
+                yield return webRequest.SendWebRequest();
+                if (webRequest.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogWarning($"Failed to download {url}: {webRequest.error}");
+                    CallbackRequests(url, null);
+                    yield break;
+                }
+                File.WriteAllBytes(tempFilePath, webRequest.downloadHandler.data);
+            }
+        }
+
+        // Load into memory (example for Texture2D)
+        Texture2D texture = LoadFromFile(tempFilePath);
+
+        _textures[url] = texture;
+        CallbackRequests(url, texture);
+    }
+
+    private static string ToLocalFilePath(string url)
+    {
+        Debug.Assert(BuildConfigLoader.IsLoaded, "BuildConfigLoader.IsLoaded");
+        string schemeWithHost = $"https://{BuildConfigLoader.Config.ApiHost}/";
+        return Path.Combine(Application.temporaryCachePath, url.Replace(schemeWithHost, ""));
+    }
+
+    private void CheckDirectory(string filePath)
+    {
+        string directory = Path.GetDirectoryName(filePath)?.Replace("\\", "/");
+        Debug.Assert(directory != null, "directory != null");
+        Debug.Log($"CheckDirectory={directory}");
+        Directory.CreateDirectory(directory);
+    }
+
+    private Texture2D LoadFromFile(string filePath)
+    {
+        Debug.Log("loadFromFile: " + filePath);
+        Texture2D tex = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+        bool success = tex.LoadImage(File.ReadAllBytes(filePath));
+
+        if (!success)
+        {
+            Debug.LogError("Failed to load texture from file!");
+        }
+
+        return tex;
     }
     
+    private void CallbackRequests(string url, Texture2D texture)
+    {
+        if (!_textureRequests.TryGetValue(url, out List<Action<Texture2D>> requests))
+        {
+            return;
+        }
+        foreach (Action<Texture2D> request in requests)
+        {
+            // TODO: requester might be destroyed, need to check
+            request?.Invoke(texture);
+        }
+        _textureRequests.Remove(url);
+    }
 }
--- Assets/Scripts/UI/MainMenu/InventoryBottleItemView.cs
+++ Assets/Scripts/UI/MainMenu/InventoryBottleItemView.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class InventoryBottleItemView : MonoBehaviour
@@ -17,29 +15,12 @@ public class InventoryBottleItemView : MonoBehaviour
         _messageJson = messageJson;
         _button.onClick.RemoveAllListeners();
         _button.onClick.AddListener(() => OnClick?.Invoke(this));
-        StartCoroutine(DownloadAndSetRawImage(messageJson.thumbnail_url));
-        
-        
-        // here we basically want: "give me the image. if it isn't on disk, download it, save it and notify me when you're done"
-        
-        
-        
-        IEnumerator DownloadAndSetRawImage(string imageUrl)
-        {
-            using var request = UnityWebRequestTexture.GetTexture(imageUrl);
-            request.SetRequestHeader("x-api-key", BuildConfigLoader.Config.ApiKey);
-            yield return request.SendWebRequest();
+        App.Instance.DownloadableContent.GetTexture(messageJson.thumbnail_url, OnTextureLoaded);
+    }
 
-            if (request.result != UnityWebRequest.Result.Success)
-            {
-                Debug.LogError($"Failed to download image: {request.error}");
-            }
-            else
-            {
-                Texture2D texture = DownloadHandlerTexture.GetContent(request);
-                _thumbnailRawImage.texture = texture;
-            }
-        }
+    private void OnTextureLoaded(Texture2D texture)
+    {
+        _thumbnailRawImage.texture = texture;
     }
 
     public BottledMessageJson Data => _messageJson;

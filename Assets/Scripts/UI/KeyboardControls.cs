3613dafd781ea4971f5925a8bf77015968c0cafb
Rene Schleese <reneschleese@gmail.com>
2025-10-26 18:34:03 +0100

    replace old input api for raw axes with new input querys for WASD

--- Assets/Scripts/UI/KeyboardControls.cs
+++ Assets/Scripts/UI/KeyboardControls.cs
@@ -30,7 +30,17 @@ public class KeyboardControls : MonoBehaviour
 
     private void TranslateKeyboardInputsToJoystick()
     {
-        Vector2 amount = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        float x = 0f;
+        float y = 0f;
+
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.aKey.isPressed) x -= 1f;
+            if (Keyboard.current.dKey.isPressed) x += 1f;
+            if (Keyboard.current.sKey.isPressed) y -= 1f;
+            if (Keyboard.current.wKey.isPressed) y += 1f;
+        }
+        Vector2 amount = new(x, y);
         amount *= 1000;
         bool hasInput = amount != Vector2.zero;
         if (!_hadJoystickInput && hasInput)

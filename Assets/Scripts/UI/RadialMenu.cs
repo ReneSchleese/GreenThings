7a9f90660e653c97264d82438dd54272138207d4
Rene Schleese <reneschleese@gmail.com>
2025-11-05 12:28:52 +0100

    add a bunch of items to radialMenu

--- Assets/Scripts/UI/RadialMenu.cs
+++ Assets/Scripts/UI/RadialMenu.cs
@@ -1,12 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class RadialMenu : MonoBehaviour
 {
     [SerializeField] private RadialMenuItem _itemPrefab;
+    [SerializeField] private Transform _itemContainer;
+    
+    private List<RadialMenuItem> _items = new();
 
     public void Init()
     {
-        RadialMenuItem radialMenuItem = Instantiate(_itemPrefab);
-        radialMenuItem.Init("Test");
+        CreateItem("Interact");
+        CreateItem("Scan");
+        CreateItem("Dig");
+        CreateItem("Toggle Mode");
+        CreateItem("Scream");
+        return;
+
+        void CreateItem(string label)
+        {
+            RadialMenuItem item = Instantiate(_itemPrefab, _itemContainer);
+            item.Init(label);
+            _items.Add(item);
+        }
     }
 }

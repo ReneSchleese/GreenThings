b9872b69fe1000d7765c4fb4dd6cd7fad20d5d28
Rene Schleese <reneschleese@gmail.com>
2025-11-05 12:44:02 +0100

    layout items

--- Assets/Scripts/UI/RadialMenu.cs
+++ Assets/Scripts/UI/RadialMenu.cs
@@ -15,6 +15,8 @@ public class RadialMenu : MonoBehaviour
         CreateItem("Dig");
         CreateItem("Toggle Mode");
         CreateItem("Scream");
+
+        LayoutItems();
         return;
 
         void CreateItem(string label)
@@ -22,6 +24,28 @@ public class RadialMenu : MonoBehaviour
             RadialMenuItem item = Instantiate(_itemPrefab, _itemContainer);
             item.Init(label);
             _items.Add(item);
+        }
+    }
+
+    private void LayoutItems()
+    {
+        if (_items == null || _items.Count == 0)
+            return;
+        
+        float stepAngle = 360f / _items.Count;
+        const bool clockwise = true;
+        const float startAngle = 90f;
+        const float radius = 150f;
+        const float dir = clockwise ? -1f : 1f;
+
+        for (int i = 0; i < _items.Count; i++)
+        {
+            float angle = startAngle + dir * stepAngle * i;
+            float rad = angle * Mathf.Deg2Rad;
+
+            Vector2 pos = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)) * radius;
+            _items[i].RectTransform.anchoredPosition = pos;
+            _items[i].RectTransform.localRotation = Quaternion.identity;
         }
     }
 }
--- Assets/Scripts/UI/RadialMenuItem.cs
+++ Assets/Scripts/UI/RadialMenuItem.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class RadialMenuItem : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _label;
+    [SerializeField] private RectTransform _rectTransform;
 
     public void Init(string label)
     {
         _label.text = label;
     }
+
+    public RectTransform RectTransform => _rectTransform;
 }

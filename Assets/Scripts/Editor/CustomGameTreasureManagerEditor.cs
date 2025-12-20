0fdb02be588dc65970bfd4c8e80ae4b17796082a
Rene Schleese <reneschleese@gmail.com>
2025-12-20 23:20:47 +0100

    make function actually spawn gameobjects and set their references

--- Assets/Scripts/Editor/CustomGameTreasureManagerEditor.cs
+++ Assets/Scripts/Editor/CustomGameTreasureManagerEditor.cs
@@ -1,12 +1,14 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [CustomEditor(typeof(GameTreasureManager))]
 public class CustomGameTreasureManagerEditor : Editor
 {
+    private const string BURIED_TREASURE_SPAWN = "BuriedTreasureSpawn";
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
@@ -19,7 +21,7 @@ public class CustomGameTreasureManagerEditor : Editor
         if (GUILayout.Button("Find and set all treasure-spawns"))
         {
             Transform[] spawns = manager.TreasureSpawnsParent.GetComponentsInChildren<Transform>()
-                .Where(spawn => spawn.name.StartsWith("BuriedTreasureSpawn")).ToArray();
+                .Where(spawn => spawn.name.StartsWith(BURIED_TREASURE_SPAWN)).ToArray();
             Undo.RecordObject(manager, "Set Treasure Spawns");
             manager.TreasureSpawns = spawns.ToArray();
             EditorUtility.SetDirty(manager);
@@ -44,23 +46,27 @@ public class CustomGameTreasureManagerEditor : Editor
         const float stepSize = 2f;
         const float height = 100f;
         const float minY = -5f;
+        List<Transform> treasureSpawns = new List<Transform>();
         for (float x = gridMin.x; x < gridMax.x; x += stepSize)
         {
             for (float z = gridMin.y; z < gridMax.y; z += stepSize)
             {
                 Vector3 origin = new(x, height, z);
                 Physics.Raycast(origin, Vector3.down, out RaycastHit hit, height - minY, LayerMask.GetMask("Environment"));
-                if (hit.collider != null && hit.collider.TryGetComponent(out EnvironmentObject envObj) && envObj.AllowsTreasureSpawn)
+                if (hit.collider == null || !hit.collider.TryGetComponent(out EnvironmentObject envObj) || !envObj.AllowsTreasureSpawn)
                 {
-                    Debug.DrawRay(hit.point, Vector3.up * 2, Color.green, 3f);   
+                    continue;
                 }
+                GameObject spawn = new(BURIED_TREASURE_SPAWN);
+                spawn.transform.position = hit.point;
+                spawn.transform.parent = manager.TreasureSpawnsParent;
+                treasureSpawns.Add(spawn.transform);
+                Debug.DrawRay(hit.point, Vector3.up * 2, Color.green, 3f);
             }
         }
-            
-        Transform[] spawns = manager.TreasureSpawnsParent.GetComponentsInChildren<Transform>()
-            .Where(spawn => spawn.name.StartsWith("BuriedTreasureSpawn")).ToArray();
+        
         Undo.RecordObject(manager, "Set Treasure Spawns");
-        manager.TreasureSpawns = spawns.ToArray();
+        manager.TreasureSpawns = treasureSpawns.ToArray();
         EditorUtility.SetDirty(manager);
     }
 }

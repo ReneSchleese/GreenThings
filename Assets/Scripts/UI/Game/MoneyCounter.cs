a7b4728323bd827e92cc52a8761602bd9d67d4b4
Rene Schleese <reneschleese@gmail.com>
2026-01-31 16:55:10 +0100

    transfer money and update ui

--- Assets/Scripts/UI/Game/MoneyCounter.cs
+++ Assets/Scripts/UI/Game/MoneyCounter.cs
@@ -1,4 +1,6 @@
-﻿using TMPro;
+﻿using System;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class MoneyCounter : MonoBehaviour
@@ -7,6 +9,8 @@ public class MoneyCounter : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _moneyTmPro, _addendTmPro;
 
     private FadeableCanvasGroup _rootFader;
+    private Tween _fadeTween;
+    private MoneyTransfer _moneyTransfer;
 
     public void Init()
     {
@@ -22,6 +26,68 @@ public class MoneyCounter : MonoBehaviour
 
     private void OnPlayerCollectedCoins(int amount)
     {
+        amount = UnityEngine.Random.Range(25, 55);
         Debug.Log($"collected {amount}");
+        if (_fadeTween is { active: true })
+        {
+            _fadeTween.Kill();
+        }
+
+        DOTween.Kill(this);
+        _fadeTween = _rootFader.Fade(true);
+        int currentBankAmount = App.Instance.UserData.Money;
+        
+        if (_moneyTransfer is null)
+        {
+            _moneyTransfer = new MoneyTransfer(amount, bankAmount: currentBankAmount);
+            _moneyTransfer.UpdateUICallback = UpdateCounters;
+        }
+        else
+        {
+            _moneyTransfer.Add(amount);
+        }
+        
+        UpdateCounters(_moneyTransfer.BankAmountSnapShot, _moneyTransfer.Addend);
+        
+        Sequence sequence = DOTween.Sequence().SetId(this);
+        sequence.AppendInterval(2f);
+        sequence.Append(_moneyTransfer.TransferGold(duration: 2f));
+        sequence.AppendCallback(() => _moneyTransfer = null);
+        sequence.AppendCallback(() => { _fadeTween = _rootFader.Fade(fadeIn: false); });
+    }
+
+    private void UpdateCounters(int bank, int addend)
+    {
+        _moneyTmPro.text = bank.ToString();
+        _addendTmPro.text = $"+{addend}";
+    }
+
+    private class MoneyTransfer
+    {
+        public MoneyTransfer(int amount, int bankAmount)
+        {
+            Addend = amount;
+            BankAmountSnapShot = bankAmount;
+        }
+
+        public void Add(int amount)
+        {
+            Addend += amount;
+        }
+        
+        public Tween TransferGold(float duration)
+        {
+            return DOVirtual.Float(0f, 1f, duration, OnUpdate);
+
+            void OnUpdate(float progress)
+            {
+                int remainingAddend = Mathf.RoundToInt(Mathf.Lerp(Addend, 0, progress));
+                UpdateUICallback?.Invoke(BankAmountSnapShot + (Addend - remainingAddend), remainingAddend);
+            }
+        }
+        
+        public Action<int, int> UpdateUICallback { get; set; }
+        public int Addend { get; private set; }
+        public int BankAmountSnapShot { get; }
     }
 }

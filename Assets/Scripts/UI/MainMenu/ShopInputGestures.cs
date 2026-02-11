a9dbdef5cc5212896dfca57a85bc25d2c1da4411
Rene Schleese <reneschleese@gmail.com>
2026-02-11 23:17:11 +0100

    add mathematics to asmdef. make a capsule follow the spline when dragging

--- Assets/Scripts/UI/MainMenu/ShopInputGestures.cs
+++ Assets/Scripts/UI/MainMenu/ShopInputGestures.cs
@@ -1,8 +1,14 @@
-﻿using UnityEngine;
+﻿using Unity.Mathematics;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Splines;
 
 public class ShopInputGestures : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    [SerializeField] private SplineContainer _splineContainer;
+    [SerializeField] private Transform _bottleTarget;
+    private float _normalizedPosition;
+    
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("OnPointerClick");
@@ -11,6 +17,14 @@ public class ShopInputGestures : MonoBehaviour, IPointerClickHandler, IDragHandl
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("OnDrag");
+        _normalizedPosition = Mathf.Clamp01(_normalizedPosition + eventData.delta.x * 0.001f);
+        
+        var spline = _splineContainer.Spline;
+        float3 position = spline.EvaluatePosition(_normalizedPosition);
+        float3 tangent = spline.EvaluateTangent(_normalizedPosition);
+
+        _bottleTarget.localPosition = position;
+        _bottleTarget.localRotation = Quaternion.LookRotation(tangent);
     }
 
     public void OnBeginDrag(PointerEventData eventData)

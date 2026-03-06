6916cc9519e86ca65a7eb4835e8af0a2684d443a
Rene Schleese <reneschleese@gmail.com>
2026-03-06 20:03:07 +0100

    try out another algorithm in order to fix the jumping bottle items

--- Assets/Scripts/UI/MainMenu/Shop/ShopCarousel.cs
+++ Assets/Scripts/UI/MainMenu/Shop/ShopCarousel.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,13 +9,15 @@ public class ShopCarousel : MonoBehaviour, IPointerClickHandler, IDragHandler, I
 {
     [SerializeField] private SplineContainer _splineContainer;
     [SerializeField] private Transform _bottleTarget;
-    [SerializeField] private CarouselBottle[] _visibleBottles;
+    [SerializeField] private List<CarouselBottle> _visibleBottles;
     [SerializeField] private float _dragSensitivity = 0.01f;
     [SerializeField] private float _damping = 5f;
     [SerializeField] private float _minVelocity = 0.01f;
+    
     private float _carouselPosition;
     private float _velocity;
     private bool _isDragging;
+    private int _currentBaseIndex;
 
     public void Init()
     {
@@ -60,10 +63,41 @@ public class ShopCarousel : MonoBehaviour, IPointerClickHandler, IDragHandler, I
 
     private void UpdateBottles()
     {
-        int visibleBottleCount = _visibleBottles.Length;
+        int visibleBottleCount = _visibleBottles.Count;
         int totalItemCount = App.Instance.Shop.Messages.Count;
         Spline spline = _splineContainer.Spline;
-        int baseIndex = Mathf.FloorToInt(_carouselPosition);
+        
+        int newBaseIndex = Mathf.FloorToInt(_carouselPosition);
+
+        if (newBaseIndex != _currentBaseIndex)
+        {
+            int direction = newBaseIndex > _currentBaseIndex ? 1 : -1;
+
+            if (direction > 0)
+            {
+                // right scroll → leftmost slot gets new data
+                int slotIndex = Mod(newBaseIndex, visibleBottleCount);
+
+                int newDataIndex = Mod(newBaseIndex + visibleBottleCount - 1, totalItemCount);
+                string url = App.Instance.Shop.Messages[newDataIndex].thumbnail_url;
+                _visibleBottles[slotIndex].Url = url;
+                App.Instance.DownloadableContent.RequestTexture(url);
+            }
+            else
+            {
+                // left scroll → rightmost slot gets new data
+                int slotIndex = Mod(newBaseIndex, visibleBottleCount);
+
+                int newDataIndex = Mod(newBaseIndex, totalItemCount);
+                string url = App.Instance.Shop.Messages[newDataIndex].thumbnail_url;
+                _visibleBottles[slotIndex].Url = url;
+                App.Instance.DownloadableContent.RequestTexture(url);
+            }
+
+            _currentBaseIndex = newBaseIndex;
+        }
+
+        
         for (var bottleIndex = 0; bottleIndex < visibleBottleCount; bottleIndex++)
         {
             float splineT = Mathf.Repeat((_carouselPosition + bottleIndex) / visibleBottleCount, 1f);
@@ -72,11 +106,6 @@ public class ShopCarousel : MonoBehaviour, IPointerClickHandler, IDragHandler, I
             CarouselBottle bottle = _visibleBottles[bottleIndex];
             bottle.transform.localPosition = pos;
             bottle.transform.localRotation = Quaternion.LookRotation(tangent);
-         
-            int dataIndex = Mod(baseIndex + bottleIndex, totalItemCount);
-            string url = App.Instance.Shop.Messages[dataIndex].thumbnail_url;
-            bottle.Url = url;
-            App.Instance.DownloadableContent.RequestTexture(url);
         }
 
         return;
